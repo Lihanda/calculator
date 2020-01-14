87925e90c02a99f23d0072a0c744dd707e9b0ca8
Davies <lihandadavies@gmail.com>
2020-01-14 10:50:46 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+<div id="app">
+  <Calculator/>
+</div>
+  
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+
+  name:'app',
+
+  components:{
+
+    Calculator
+
+  }
+
+
+  }
+   
+</script>
+
+<style scoped>
+
+
+</style>
+ 
